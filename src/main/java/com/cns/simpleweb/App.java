44d18e0ec9bb8959c9a5701445a24ca0be0c5282
Chandan Kumar <kumar.chandan@mastek.com>
2024-09-23 16:12:05 +0100

    unused import

--- src/main/java/com/cns/simpleweb/App.java
+++ src/main/java/com/cns/simpleweb/App.java
@@ -1,9 +1,8 @@
 package com.cns.simpleweb;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 public class App 
 {
